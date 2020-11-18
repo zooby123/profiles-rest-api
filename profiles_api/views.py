b40764f80b005f531d7c6f4356d6052a9ed1c01c
zooby123 <zed-786@hotmail.com>
2020-11-18 21:35:21 +0000

    added searching profiles

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from profiles_api import serializers
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import viewsets
+from rest_framework import filters
 from profiles_api import models
 from profiles_api import permissions
 
@@ -101,3 +102,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
